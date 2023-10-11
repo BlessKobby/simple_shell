49d23521457acabce98e5334342b22c136c6494c
BlessKobby <amablebless@gmail.com>
2023-10-11 14:46:02 -0700

    Debugging

--- command_exec.c
+++ command_exec.c
@@ -1,7 +1,14 @@
 #include "shell.h"
 
+/**
+ * command_exec -  Execute a program as new process.
+ * @execute: The command to be executed.
+ *
+ * Return: No return value. Program is executed or errors handled.
+*/
 void command_exec(const char *execute)
 {
+
 	pid_t ch_pid = fork();
 
 	if (ch_pid == -1)
@@ -13,7 +20,7 @@ void command_exec(const char *execute)
 	{
 		char *args[] = {execute, NULL};
 		char *envp[] = {NULL};
-        
+
 		if (execve(execute, args, envp) == -1)
 		{
 			perror("execve");
--- shell.c
+++ shell.c
@@ -1,15 +1,18 @@
 #include "shell.h"
-
+/**
+ * main - Entry point for program.
+ * Return: Always return 0.
+ */
 int main(void)
 {
-    char command[140];
+	char command[140];
 
-    for (;;)
-    {
-        _prompt();
-        read_command(command, sizeof(command));
-        command_exec(command);
-    }
+	while (1)
+	{
+		_prompt();
+		read_command(command, sizeof(command));
+		command_exec(command);
+	}
 
-    return 0;
+	return (0);
 }
